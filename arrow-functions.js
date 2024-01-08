ac6d8804896f7a289ae387e9c64b4c0284c74b05
Oliver Sharp <olliesharp5@gmail.com>
2024-01-08 16:47:36 +0000

    Completed lesson content for arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => {
+    // Code block
+    return a + b;
+}
+
+let sum2 = addTwoNumbers2(3, 5);
+console.log(sum2);
+
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a, b) => a + b;
+
+let sum3 = addTwoNumbers2(6, 4);
+console.log(sum3);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello There!");
+
+const sayHello = () => console.log("hello");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    "<p> This is a multiline string <p>"
+)
+console.log(returnMultipleLines())
